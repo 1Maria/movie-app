dee14f69599e7c718b8fbaabce446d0b125d6a4a
Maria <mliberov@gmail.com>
2020-08-06 19:01:41 -0400

    Add search endpoint

--- server/index.js
+++ server/index.js
@@ -19,9 +19,17 @@ app.get('/movies/currently_playing', async (req, res) => {
     res.json(currentlyPlaying);
 });
 
-app.get('/movies/search', (req, res) => {
-    res.send('Hello World!')
-})
+app.get('/movies/search', async (req, res) => {
+    let { page, query } = req.query;
+
+    const pageInt = parseInt(page);
+    // Ensure page conforms to API restrictions 
+    page = pageInt < 1 || pageInt > 1000 ? 1 : page;
+
+    const movieResponse = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${process.env.APIKEY}&query=${query}&page=${page}`);
+    const search = await movieResponse.json();
+    res.json(search);
+});
 
 app.listen(8000, () => {
     console.log('Node.js listening on port ' + 8000)
