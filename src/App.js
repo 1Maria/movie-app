d66c1bb6bbbd437a16b41b151464194dec404f90
Maria <mliberov@gmail.com>
2020-08-07 10:08:26 -0400

    Remove default styles

--- src/App.js
+++ src/App.js
@@ -20,12 +20,6 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-        </header>
         <div className="movies">
           <button onClick={ this.getCurrentlyPlaying }>Get Currently Playing</button>
           <span className="currently-playing">{ JSON.stringify(this.state.movies) }</span>
