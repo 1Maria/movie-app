377ed71e60dfa740166782e596fb2885132f247a
Maria <mliberov@gmail.com>
2020-08-07 14:44:39 -0400

    Implement paginate and hook it up to currently playing movies

--- src/App.js
+++ src/App.js
@@ -3,12 +3,36 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { Container, Row } from 'react-bootstrap';
 import Movie from "./Movie";
+import ReactPaginate from 'react-paginate';
 
 class App extends React.Component {
 
   state = {
     movies: {
       results: []
+    },
+    paginationConfig: {
+      previousLabel: 'previous',
+      nextLabel: 'next',
+      breakLabel: '...',
+      breakClassName: 'page-item',
+      pageCount: 1,
+      marginPagesDisplayed: 2,
+      pageRangeDisplayed: 5,
+      pageClassName: 'page-item',
+      previousClassName: 'page-item',
+      nextClassName: 'page-item',
+      pageLinkClassName: 'page-link',
+      previousLinkClassName: 'page-link',
+      nextLinkClassName: 'page-link',
+      breakLinkClassName: 'page-link',
+      onPageChange: (page) => {
+        this.getCurrentlyPlaying(page.selected + 1);
+      },
+      containerClassName: 'pagination',
+      subContainerClassName: 'pages pagination', 
+      activeClassName: 'active',
+      offset: 1
     }
   };
 
@@ -16,24 +40,37 @@ class App extends React.Component {
     this.getCurrentlyPlaying();
   }
 
-  getCurrentlyPlaying = async () => {
-    const movieResponse = await fetch(`/movies/currently_playing`);
+  getCurrentlyPlaying = async (page = 1) => {
+    console.log(page);
+    const movieResponse = await fetch(`/movies/currently_playing?page=${page}`);
     const currentlyPlayingMovies = await movieResponse.json()
-    this.setState({
-      movies: currentlyPlayingMovies
+    this.setState(prevState => {
+      return {
+        movies: currentlyPlayingMovies,
+        paginationConfig: {
+          ...prevState.paginationConfig,
+          offset: currentlyPlayingMovies.page,
+          pageCount: currentlyPlayingMovies.total_pages
+        }
+      }
     });
   }
 
   render() {
     return (
       <Container>
-          <Row xs={4} md={4}>
-            {this.state.movies.results.map(movie => 
-              <Movie {...movie} />
-            )}
-          </Row>
-      </Container>
 
+        <ReactPaginate {...this.state.paginationConfig} />
+
+        <Row xs={4} md={4}>
+          {this.state.movies.results.map(movie =>
+            <Movie {...movie} />
+          )}
+        </Row>
+
+        <ReactPaginate {...this.state.paginationConfig} />
+
+      </Container>
     );
   };
 }
