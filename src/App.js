2e718bc6e9713d1f2caf9f35040815f83421e580
Maria <mliberov@gmail.com>
2020-08-07 10:05:40 -0400

    Add proxy and add get movies to front end

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import fetch from 'node-fetch';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+class App extends React.Component {
+
+  state = {
+    movies: {}
+  };
+
+  getCurrentlyPlaying = async () => {
+    const movieResponse = await fetch(`/movies/currently_playing`);
+    const currentlyPlayingMovies = await movieResponse.json()
+    this.setState({
+        movies: currentlyPlayingMovies
+    });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+        </header>
+        <div className="movies">
+          <button onClick={ this.getCurrentlyPlaying }>Get Currently Playing</button>
+          <span className="currently-playing">{ JSON.stringify(this.state.movies) }</span>
+        </div>
+      </div>
+    );
+  };
 }
 
 export default App;
